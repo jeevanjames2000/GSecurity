16aa46132020aeb9d46f3c74416785520b8da474
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-01-08 11:58:34 +0530

    fines

--- screens/pages/Fines.js
+++ screens/pages/Fines.js
@@ -9,7 +9,7 @@ const HistoryCard = () => {
   return (
     <HStack
       bgColor={"#F0F4F8"}
-      paddingY={"8"}
+      paddingY={"3"}
       paddingLeft={"4"}
       paddingRight={"2"}
       alignItems={"center"}
@@ -21,7 +21,7 @@ const HistoryCard = () => {
         <Avatar
           bg="green.500"
           alignSelf="center"
-          size="lg"
+          size="sm"
           source={{
             uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
           }}
@@ -30,7 +30,7 @@ const HistoryCard = () => {
         </Avatar>
 
         <VStack>
-          <Text fontSize={"lg"} fontWeight={"bold"}>
+          <Text fontSize={"sm"} fontWeight={"bold"}>
             Kelivin Fryimnk
           </Text>
           <Text fontSize={"sm"} fontWeight={"semibold"}>
@@ -51,70 +51,70 @@ const HistoryCard = () => {
 
 export default function Fines() {
   return (
-    <SafeAreaProvider style={styles.container}>
-      <SafeAreaView>
-        <ScrollView>
-          <View>
-            <VStack space={"lg"} marginBottom={"8"}>
-              <HStack justifyContent={"space-between"} space={"md"}>
-                <Box
-                  borderColor={"#DBE0E5"}
-                  borderWidth={"1"}
-                  padding={"5"}
-                  width={"47%"}
-                  borderRadius={"lg"}
-                >
-                  <Text fontSize={"lg"} fontWeight={"medium"}>
-                    Total Fines Issued{" "}
-                  </Text>
-                  <Text fontSize={"xl"} fontWeight={"bold"}>
-                    1,200{" "}
-                  </Text>
-                </Box>
-                <Box
-                  borderColor={"#DBE0E5"}
-                  borderWidth={"1"}
-                  padding={"5"}
-                  width={"47%"}
-                  borderRadius={"lg"}
-                >
-                  <Text fontSize={"lg"} fontWeight={"medium"}>
-                    Pending Fines
-                  </Text>
-                  <Text fontSize={"xl"} fontWeight={"bold"}>
-                    320
-                  </Text>
-                </Box>
-              </HStack>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={{ flex: 1 }}>
+          <VStack space={"lg"} marginBottom={"4"}>
+            <HStack justifyContent={"space-between"} space={2}>
               <Box
                 borderColor={"#DBE0E5"}
                 borderWidth={"1"}
-                padding={"5"}
-                width={"full"}
+                padding={"2"}
+                width={"47%"}
                 borderRadius={"lg"}
               >
                 <Text fontSize={"lg"} fontWeight={"medium"}>
-                  Paid Fines
+                  Total Fines Issued{" "}
+                </Text>
+                <Text fontSize={"xl"} fontWeight={"bold"}>
+                  1,200{" "}
+                </Text>
+              </Box>
+              <Box
+                borderColor={"#DBE0E5"}
+                borderWidth={"1"}
+                padding={"2"}
+                width={"47%"}
+                borderRadius={"lg"}
+              >
+                <Text fontSize={"lg"} fontWeight={"medium"}>
+                  Pending Fines
                 </Text>
                 <Text fontSize={"xl"} fontWeight={"bold"}>
                   320
                 </Text>
               </Box>
-            </VStack>
-
-            <View>
-              <Text fontSize={"2xl"} fontWeight={"bold"} marginY={"4"}>
-                History
+            </HStack>
+            <Box
+              borderColor={"#DBE0E5"}
+              borderWidth={"1"}
+              padding={"2"}
+              width={"full"}
+              borderRadius={"lg"}
+            >
+              <Text fontSize={"lg"} fontWeight={"medium"}>
+                Paid Fines
               </Text>
-
-              <HistoryCard />
-              <HistoryCard />
-
-              <HistoryCard />
-              <HistoryCard />
-            </View>
-          </View>
-        </ScrollView>
+              <Text fontSize={"xl"} fontWeight={"bold"}>
+                320
+              </Text>
+            </Box>
+          </VStack>
+          <Text fontSize={"2xl"} fontWeight={"bold"} marginY={"2"}>
+            History
+          </Text>
+          <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+            showsVerticalScrollIndicator={false}
+            padding={"0"}
+          >
+            {Array(9)
+              .fill(0)
+              .map((_, i) => (
+                <HistoryCard key={i} />
+              ))}
+          </ScrollView>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
